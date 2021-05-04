552f06f0a133f480d578f41fcab1aaf9f3387c85
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-03 21:52:29 -0400

    Added a courses model and added a sample route to add a sample course to database

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+
+const Course = require("./models/course");
+
+mongoose.connect("mongodb://localhost:27017/lang", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 const app = express();
 
@@ -8,6 +23,16 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/course-new", async (req, res) => {
+  const course = new Course({
+    title: "French I",
+    description: "Introduction French course for beginners",
+  });
+  await course.save();
+
+  res.send(course);
 });
 
 app.listen(3000, () => {

35cfd2bc4329a9f5b726c27fda5b4d114c0455ad
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-05 18:03:04 -0400

    Added edit course page. Created routes to handle editing and deleting posts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const path = require("path");
 
 const Course = require("./models/course");
@@ -23,6 +24,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 // Courses routes
 app.get("/", (req, res) => {
@@ -53,6 +55,39 @@ app.get("/courses/:id", async (req, res) => {
   const course = await Course.findById(req.params.id);
 
   res.render("courses/course-show", { course });
+});
+
+app.get("/courses/:id/edit", async (req, res) => {
+  const course = await Course.findById(req.params.id);
+
+  res.render("courses/course-edit", { course });
+});
+
+app.put("/courses/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, price } = req.body.course;
+
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      title,
+      description,
+      price,
+    },
+    { useFindAndModify: false }
+  );
+
+  console.log(course);
+
+  res.redirect(`/courses/${course._id}`);
+});
+
+app.delete("/courses/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Course.findByIdAndRemove(id, { useFindAndModify: false });
+
+  res.redirect("/courses");
 });
 
 app.listen(3000, () => {

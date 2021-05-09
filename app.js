8420dc22030f1e8794aab86ac558dd0fbd6670c1
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-09 18:58:07 -0400

    Created a footer and used ejs-mate to create a base layout which includes both footer and navbar and will be used for all pages in application

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const ejsMate = require('ejs-mate');
 const methodOverride = require("method-override");
 const path = require("path");
 
@@ -20,6 +21,8 @@ db.once("open", () => {
 });
 
 const app = express();
+
+app.engine('ejs', ejsMate)
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
